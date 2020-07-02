a7d7d3a2b8b6da92bb7989859bd71432ebcb9347
Ashok Ballolli <ashok.ballolli123@gmail.com>
2020-07-02 20:20:35 +0200

    util file for config file reader

---
+++ utils/config.py
@@ -0,0 +1,21 @@
+import yaml
+class Config:
+    def __init__(self):
+        with open("../config/config.yaml") as f:
+            self.conf = yaml.full_load(f)
+
+    def get_property(self, key):
+        confKeys = key.split(".", maxsplit=1)
+        if len(confKeys) == 1:
+            return self.conf.get(key)
+        else:
+            return self.conf.get(confKeys[0])[confKeys[1]]
+
+
+# c = Config()
+# redis_conf = c.get_property("twitter")
+# authKey = redis_conf["consumer_key"]
+#
+# print(authKey)
+#
+# print(c.get_property("twitter.consumer_key"))
